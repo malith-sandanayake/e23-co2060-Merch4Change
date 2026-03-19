08ed3eece6da380e4286c79d69537df398bbe16e
Ashinshana <e23089@eng.pdn.ac.lk>
2026-03-19 14:07:15 +0530

    Set up Donation model

---
+++ code/Backend/src/models/Donation.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const donationSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+    projectId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Project",
+      required: true,
+      index: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    type: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: { createdAt: true, updatedAt: false },
+    versionKey: false,
+  },
+);
+
+const Donation = mongoose.model("Donation", donationSchema);
+
+export default Donation;
+
