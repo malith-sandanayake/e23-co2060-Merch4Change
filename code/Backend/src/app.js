7eb52310c285e803baf1d8b375099968565a1d9e
malith-sandanayake <malithsandanayake@gmail.com>
2026-04-15 17:51:43 +0530

    app.js

--- code/Backend/src/app.js
+++ code/Backend/src/app.js
@@ -1,13 +1,15 @@
-import cors from "cors";
+// importing external tools
+import cors from "cors";          // only allows requests with the react FrontendURL
 import express from "express";
-import helmet from "helmet";
-import morgan from "morgan";
+import helmet from "helmet";      // security package
+import morgan from "morgan";      // logger 
 
+// importing internal tools
 import env from "./config/env.js";
-import errorHandler from "./middlewares/errorHandler.js";
-import notFound from "./middlewares/notFound.js";
-import authRoutes from "./routes/auth.routes.js";
-import healthRoutes from "./routes/health.routes.js";
+import errorHandler from "./middlewares/errorHandler.js"; // if code crash- your get clean error, no secret code leakage
+import notFound from "./middlewares/notFound.js";         // if url not found 
+import authRoutes from "./routes/auth.routes.js";         // login, signup, sign out
+import healthRoutes from "./routes/health.routes.js";     // simple route to check whether server is alive and working 
 
 const app = express();
 
@@ -15,17 +17,17 @@ app.use(helmet());
 app.use(
   cors({
     origin: env.frontendUrl,
-    credentials: true,
+    credentials: true,    // allow frontend to send cookies
   }),
 );
-app.use(express.json({ limit: "1mb" }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "1mb" }));      // translate json raw text into js object
+app.use(express.urlencoded({ extended: true }));  // standard HTML form submissions
 app.use(morgan(env.nodeEnv === "production" ? "combined" : "dev"));
 
 app.use("/api/v1", healthRoutes);
 app.use("/api/v1/auth", authRoutes);
 
 app.use(notFound);
-app.use(errorHandler);
+app.use(errorHandler);  // any above route has bug or crashed Express jumps into here 
 
 export default app;
