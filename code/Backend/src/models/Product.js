fcfc61536cdf2613dd9419b06903e5ba32e90338
malith-sandanayake <malithsandanayake@gmail.com>
2026-04-28 09:34:16 +0530

    reafactor(Marketplace): Decomposed marketplace

--- code/Backend/src/models/Product.js
+++ code/Backend/src/models/Product.js
@@ -36,6 +36,10 @@ const productSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    imageUrl: {
+      type: String,
+      default: "",
+    },
   },
   {
     timestamps: true,
