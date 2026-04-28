5e3ebf211ea11e9524d85676dab055d1c68a40ce
Ashinshana <e23089@eng.pdn.ac.lk>
2026-04-28 09:55:27 +0530

    feat(controller): save cloudinary image urls on product create

---
+++ code/Backend/src/controllers/product.controller.js
@@ -0,0 +1,31 @@
+import Product from "../models/Product.js";
+
+export const createProduct = async (req, res, next) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json({ success: true, data: product });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getAllProducts = async (req, res, next) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json({ success: true, count: products.length, data: products });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getProductById = async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ success: false, message: "Product not found" });
+    }
+    res.status(200).json({ success: true, data: product });
+  } catch (error) {
+    next(error);
+  }
+};
