2307567e580200c4a85a0462ccc23010d0403609
malith-sandanayake <malithsandanayake@gmail.com>
2026-05-01 11:25:53 +0530

    backend coin-system routes done

--- code/Backend/src/routes/auth.routes.js
+++ code/Backend/src/routes/auth.routes.js
@@ -1,16 +1,15 @@
 import { Router } from "express";
 
-import { checkUsernameAvailability, login, register } from "../controllers/auth.controller.js";
+import { checkUsernameAvailability, login, register, logout } from "../controllers/auth.controller.js";
 import validateRequest from "../middlewares/validateRequest.js";
 import { validateLoginBody, validateRegisterBody } from "../validators/auth.validator.js";
 
 const router = Router();
 
-
-
 router.post("/register", validateRequest({ body: validateRegisterBody }), register);
 router.get("/username-availability", checkUsernameAvailability);
 router.post("/login", validateRequest({ body: validateLoginBody }), login);
+router.post("/logout", logout);
 
 
 export default router;
